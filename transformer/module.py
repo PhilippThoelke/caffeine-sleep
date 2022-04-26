708dd4edf1d374b854b3e7c524836d4713a48304
Philipp Thölke <philipp.thoelke@posteo.de>
2022-04-25 21:07:13 -0400

    Implement basic transformer architecture

--- transformer/module.py
+++ transformer/module.py
@@ -1,3 +1,4 @@
+import math
 import torch
 from torch import nn, optim
 import torch.nn.functional as F
@@ -5,12 +6,47 @@ import pytorch_lightning as pl
 
 
 class TransformerModule(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, hparams):
         super().__init__()
-        self.mlp = nn.Sequential(nn.Linear(20, 8), nn.ReLU(), nn.Linear(8, 3))
+        self.save_hyperparameters(hparams)
+
+        self.window_length = 5120 // self.hparams.num_tokens
+        self.proj = nn.Linear(self.window_length, self.hparams.embedding_dim)
+        self.pe = PositionalEncoding(
+            self.hparams.embedding_dim, dropout=self.hparams.dropout
+        )
+        self.register_parameter(
+            "class_token", nn.Parameter(torch.randn(self.hparams.embedding_dim))
+        )
+        encoder_layer = nn.TransformerEncoderLayer(
+            self.hparams.embedding_dim,
+            nhead=8,
+            dim_feedforward=self.hparams.embedding_dim * 2,
+            dropout=self.hparams.dropout,
+        )
+        self.encoder = nn.TransformerEncoder(encoder_layer, self.hparams.num_layers)
+        self.outnet = nn.Sequential(
+            nn.Linear(self.hparams.embedding_dim, 64), nn.ReLU(), nn.Linear(64, 1)
+        )
 
     def forward(self, x):
-        return torch.sigmoid(self.mlp(x.norm(dim=1)))
+        # crop sequence to be divisible by the desired number of tokens
+        x = x[:, : self.hparams.num_tokens * self.window_length]
+        # reshape x from (B x time x elec) to (token x B x window_length)
+        x = x.view(x.size(0), self.hparams.num_tokens, self.window_length, x.size(2))
+        x = x.permute(0, 3, 1, 2).reshape(x.size(0), -1, self.window_length)
+        x = x.permute(1, 0, 2)
+        # linear projection into embedding dimension
+        x = self.proj(x)
+        # add positional encoding
+        x = self.pe(x)
+        # prepend class token to the sequence
+        x = torch.cat([self.class_token[None, None].repeat(1, x.size(1), 1), x], dim=0)
+        # pass sequence through the transformer
+        x = self.encoder(x)
+        # extract class token and apply output model
+        x = self.outnet(x[0])
+        return x
 
     def training_step(self, batch, batch_idx):
         return self.step(batch, batch_idx, running_stage="train")
@@ -20,33 +56,40 @@ class TransformerModule(pl.LightningModule):
 
     def step(self, batch, batch_idx, running_stage):
         x, condition, stage, subject = batch
-        condition_pred, stage_pred, subject_pred = self.mlp(x.norm(dim=1)).T
+        pred = self(x).squeeze()
 
-        # losses
-        condition_loss = F.binary_cross_entropy_with_logits(
-            condition_pred, condition.float()
-        )
-        stage_loss = F.binary_cross_entropy_with_logits(stage_pred, stage.float())
-        subject_loss = F.binary_cross_entropy_with_logits(subject_pred, subject.float())
-
-        self.log(f"{running_stage}_condition_loss", condition_loss)
-        self.log(f"{running_stage}_stage_loss", stage_loss)
-        self.log(f"{running_stage}_subject_loss", subject_loss)
+        # loss
+        loss = F.binary_cross_entropy_with_logits(pred, condition.float())
+        self.log(f"{running_stage}_loss", loss)
 
         # accuracy
-        condition_acc = (
-            ((torch.sigmoid(condition_pred) > 0.5).int() == condition).float().mean()
-        )
-        stage_acc = ((torch.sigmoid(stage_pred) > 0.5).int() == stage).float().mean()
-        subject_acc = (
-            ((torch.sigmoid(subject_pred) > 0.5).int() == subject).float().mean()
-        )
+        acc = ((torch.sigmoid(pred) > 0.5).int() == condition).float().mean()
+        self.log(f"{running_stage}_acc", acc)
 
-        self.log(f"{running_stage}_condition_acc", condition_acc)
-        self.log(f"{running_stage}_stage_acc", stage_acc)
-        self.log(f"{running_stage}_subject_acc", subject_acc)
-
-        return condition_loss + 0 * stage_loss + 0 * subject_loss
+        return loss
 
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=1e-4)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model)
+        )
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[: x.size(0)]
+        return self.dropout(x)
--- transformer/train.py
+++ transformer/train.py
@@ -7,23 +7,25 @@ from dataset import RawDataset
 from module import TransformerModule
 
 
-def main(data_path, label_path, val_ratio=0.2):
+def main(args):
     # load data
-    data = RawDataset(data_path, label_path)
+    data = RawDataset(args.data_path, args.label_path)
     idxs = torch.randperm(len(data))
 
     # train subset
-    idx_train = idxs[: -int(len(data) * val_ratio)]
+    idx_train = idxs[: -int(len(data) * args.val_ratio)]
     train_data = Subset(data, idx_train)
-    train_dl = DataLoader(train_data, batch_size=8, shuffle=True, num_workers=4)
+    train_dl = DataLoader(
+        train_data, batch_size=args.batch_size, shuffle=True, num_workers=4
+    )
 
     # val subset
-    idx_val = idxs[-int(len(data) * val_ratio) :]
+    idx_val = idxs[-int(len(data) * args.val_ratio) :]
     val_data = Subset(data, idx_val)
-    val_dl = DataLoader(val_data, batch_size=64, num_workers=4)
+    val_dl = DataLoader(val_data, batch_size=args.batch_size, num_workers=4)
 
     # define model
-    module = TransformerModule()
+    module = TransformerModule(args)
 
     # train
     trainer = pl.Trainer(accelerator="auto", devices="auto")
@@ -44,6 +46,42 @@ if __name__ == "__main__":
         required=True,
         help="path to the csv file containing labels",
     )
-    args = parser.parse_args()
+    parser.add_argument(
+        "--batch-size",
+        default=32,
+        type=int,
+        help="batch size",
+    )
+    parser.add_argument(
+        "--val-ratio",
+        default=0.2,
+        type=float,
+        help="ratio of the data to be used for validation",
+    )
+    parser.add_argument(
+        "--num-tokens",
+        default=20,
+        type=int,
+        help="number of temporal tokens the 20s EEG is split into",
+    )
+    parser.add_argument(
+        "--embedding-dim",
+        default=256,
+        type=int,
+        help="dimension of tokens inside the transformer",
+    )
+    parser.add_argument(
+        "--num-layers",
+        default=6,
+        type=int,
+        help="number of encoder layers in the transformer",
+    )
+    parser.add_argument(
+        "--dropout",
+        default=0.1,
+        type=float,
+        help="dropout ratio",
+    )
 
-    main(args.data_path, args.label_path)
+    args = parser.parse_args()
+    main(args)
