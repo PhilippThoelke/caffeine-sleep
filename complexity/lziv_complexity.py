130c5e4c5a4f5c96254dc3a244246bdc02a7a406
Philipp Thölke <philipp.thoelke@posteo.de>
2022-05-05 14:55:23 -0400

    Add a different lziv implementation

--- complexity/lziv_complexity.py
+++ complexity/lziv_complexity.py
@@ -5,6 +5,7 @@ import pickle
 import numpy as np
 from scipy import signal
 from antropy import lziv_complexity
+from neurokit2.complexity import complexity_lempelziv
 from joblib import Parallel, delayed
 from tqdm import tqdm
 from mne import viz, stats
@@ -14,6 +15,7 @@ data_dir = "transformer/data/200/"
 result_dir = "results/lziv/"
 
 use_psd = False
+use_neurokit = True
 
 stages = ["AWSL", "NREM", "REM"]
 conditions = ["CAF", "PLAC"]
@@ -23,11 +25,16 @@ def get_paths(root, subject="*", stage="*", condition="*"):
     return glob.glob(join(root, f"{subject}n*_{stage}_*_{condition}.npy"))
 
 
-def complexity(epoch, use_psd=True):
+def complexity(epoch, use_psd=False, use_neurokit=False):
     if use_psd:
         # TODO: maybe use something other than median split to binarize the PSD
-        epoch = signal.welch(epoch, fs=256, nperseg=len(epoch) // 6, noverlap=0, window='hamming')[1]
-    return lziv_complexity((epoch > np.median(epoch)).astype(int), normalize=True)
+        epoch = signal.welch(
+            epoch, fs=256, nperseg=len(epoch) // 6, noverlap=0, window="hamming"
+        )[1]
+    if use_neurokit:
+        return complexity_lempelziv(epoch, normalize=True)[0]
+    else:
+        return lziv_complexity((epoch > np.median(epoch)).astype(int), normalize=True)
 
 
 result = {}
@@ -58,7 +65,9 @@ for stage in stages:
             if subject in drop_subjects:
                 continue
             dat = data[condition][subject].transpose((0, 2, 1)).reshape(-1, 5120)
-            res = Parallel(n_jobs=-1)(delayed(complexity)(epoch, use_psd) for epoch in dat)
+            res = Parallel(n_jobs=-1)(
+                delayed(complexity)(epoch, use_psd, use_neurokit) for epoch in dat
+            )
             result[stage][condition].append(np.array(res).reshape(-1, 20).mean(axis=0))
         result[stage][condition] = np.stack(result[stage][condition])
 
