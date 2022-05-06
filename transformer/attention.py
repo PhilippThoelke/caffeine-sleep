1feb90a66617596500696856649d7ba134a3f156
Philipp Thölke <philipp.thoelke@posteo.de>
2022-05-06 16:44:18 -0400

    Time averaged attention maps with t-tests

--- transformer/attention.py
+++ transformer/attention.py
@@ -8,6 +8,7 @@ import torch
 from torch.nn import TransformerEncoderLayer
 from torch.utils.data import DataLoader
 from mne.viz import plot_topomap
+from mne.stats import permutation_t_test
 from dataset import RawDataset
 from module import TransformerModule
 
@@ -51,7 +52,9 @@ def main(args, rollout=True):
     torch.set_grad_enabled(False)
 
     # load model
-    model = TransformerModule.load_from_checkpoint(args.model_path).eval()
+    model = TransformerModule.load_from_checkpoint(
+        args.model_path, epoch_length=5120
+    ).eval()
     model.freeze()
     model = attention_wrapper(model)
 
@@ -64,7 +67,7 @@ def main(args, rollout=True):
     for x, y, _, _ in tqdm(dl, desc="extracting attention weights"):
         pred, w = model(x)
 
-        mask = (torch.sigmoid(pred.squeeze()) < 0.5).int() == y
+        mask = (pred.squeeze() < 0.5).int() == y
         w = w[:, mask]
         y = y[mask]
 
@@ -73,7 +76,7 @@ def main(args, rollout=True):
 
         weights.append(w)
         labels.append(y)
-        if len(weights) > 5:
+        if len(weights) > 10:
             break
     labels = torch.cat(labels)
 
@@ -99,17 +102,71 @@ def main(args, rollout=True):
         vmin = min(elecs1.min(), elecs2.min())
         vmax = max(elecs1.max(), elecs2.max())
 
+        # channels and temporal
         fig, axes = plt.subplots(nrows=2, ncols=20)
-        for elec, ax in zip(elecs1, axes[0]):
+        for elec, ax in zip(elecs1.T, axes[0]):
             plot_topomap(
                 elec, pos, axes=ax, show=False, contours=False, vmin=vmin, vmax=vmax
             )
-        for elec, ax in zip(elecs2, axes[1]):
+        for elec, ax in zip(elecs2.T, axes[1]):
             plot_topomap(
                 elec, pos, axes=ax, show=False, contours=False, vmin=vmin, vmax=vmax
             )
         axes[0, 0].set_ylabel(data.id2condition(0))
         axes[1, 0].set_ylabel(data.id2condition(1))
+
+        # averaged temporally
+        fig, axes = plt.subplots(ncols=2)
+        plot_topomap(
+            elecs1.max(dim=1).values,
+            pos,
+            axes=axes[0],
+            show=False,
+            contours=False,
+            vmin=vmin,
+            vmax=vmax,
+        )
+        plot_topomap(
+            elecs2.max(dim=1).values,
+            pos,
+            axes=axes[1],
+            show=False,
+            contours=False,
+            vmin=vmin,
+            vmax=vmax,
+        )
+        axes[0].set_title(data.id2condition(0))
+        axes[1].set_title(data.id2condition(1))
+
+        # averaged temporally, caf - plac
+        caf_id = int(data.id2condition(0) == "PLAC")
+        w = (
+            weights.mean(dim=1)[:, 1:]
+            .reshape(-1, 20, model.model.hparams.num_tokens)
+            .max(dim=2)
+            .values
+        )
+        caf = w[labels == caf_id]
+        plac = w[labels == (1 - caf_id)]
+        mincount = min(len(caf), len(plac))
+        caf = caf[:mincount].numpy()
+        plac = plac[:mincount].numpy()
+        tval, pval, _ = permutation_t_test(caf - plac, n_permutations=1000, n_jobs=-1)
+
+        plt.figure()
+        plt.title("CAF - PLAC")
+
+        absmax = max(abs(tval.min()), abs(tval.max()))
+        plot_topomap(
+            tval,
+            pos,
+            mask=pval < 0.05,
+            show=False,
+            contours=False,
+            vmin=-absmax,
+            vmax=absmax,
+            cmap="coolwarm",
+        )
         plt.show()
     else:
         # plot weights without rollout
