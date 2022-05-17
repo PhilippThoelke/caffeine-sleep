278bb6664daf4fba3e6ee6e20d989af0ee5666ea
Philipp Thölke <philipp.thoelke@posteo.de>
2022-05-17 01:09:14 -0400

    Implement fist movement target

--- transformer/get_example_dset.py
+++ transformer/get_example_dset.py
@@ -6,38 +6,69 @@ from mne.io import concatenate_raws, read_raw_edf
 from mne.datasets import eegbci
 
 
-subjects = range(1, 110)
-runs = [1, 2]
-run_labels = {1: "baseline eyes open", 2: "baseline eyes closed"}
-epoch_duration = 20
 result_dir = "transformer/data/"
+target_type = "fist-motion"  # baseline-eyes, fist-motion
 
 
-epochs = []
-subject_labels = []
-labels = []
-for subject in subjects:
-    for run in runs:
-        raw_fnames = eegbci.load_data(subject, run)
+def extract_baseline_eyes(subjects, runs, epoch_duration):
+    epochs = []
+    subject_labels = []
+    labels = []
+    for subject in subjects:
+        for run in runs:
+            raw_fnames = eegbci.load_data(subject, run)
+            raws = [read_raw_edf(f, preload=True) for f in raw_fnames]
+            raw = concatenate_raws(raws)
+            data = raw.get_data()
+            epoch_steps = int(epoch_duration * raw.info["sfreq"])
+
+            offset = 0
+            for _ in range(data.shape[1] // epoch_steps):
+                epochs.append(data[:, offset : offset + epoch_steps].astype(np.float32))
+                subject_labels.append(subject)
+                labels.append(run)
+                offset += epoch_steps
+    return epochs, subject_labels, labels
+
+
+def extract_fist_motion(subjects, runs, epoch_duration):
+    epochs = []
+    subject_labels = []
+    labels = []
+    for subject in subjects:
+        raw_fnames = eegbci.load_data(subject, runs)
         raws = [read_raw_edf(f, preload=True) for f in raw_fnames]
         raw = concatenate_raws(raws)
-        data = raw.get_data()
-        epoch_steps = int(epoch_duration * raw.info["sfreq"])
 
-        offset = 0
-        for _ in range(data.shape[1] // epoch_steps):
-            epochs.append(data[:, offset : offset + epoch_steps].astype(np.float32))
-            subject_labels.append(subject)
-            labels.append(run)
-            offset += epoch_steps
+        curr_epochs = mne.Epochs(
+            raw, *raw.find_edf_events(), tmin=-1, tmax=epoch_duration - 1, preload=True
+        )
+        if curr_epochs.get_data().shape[-1] != (epoch_duration * 160 + 1):
+            # skip runs which don't have a sampling frequency of 160
+            continue
+        # left fist
+        epochs.extend(list(curr_epochs["T1"].get_data()))
+        subject_labels.extend([subject] * len(curr_epochs["T1"]))
+        labels.extend(["left fist"] * len(curr_epochs["T1"]))
+        # right fist
+        epochs.extend(list(curr_epochs["T2"].get_data()))
+        subject_labels.extend([subject] * len(curr_epochs["T2"]))
+        labels.extend(["right fist"] * len(curr_epochs["T2"]))
+    return epochs, subject_labels, labels
+
+
+def extract_epochs(target, subjects=range(1, 110)):
+    if target == "baseline-eyes":
+        return extract_baseline_eyes(subjects=subjects, runs=[1, 2], epoch_duration=5)
+    elif target == "fist-motion":
+        return extract_fist_motion(subjects=subjects, runs=[3, 7, 11], epoch_duration=5)
+    raise ValueError(f"Unrecognized target {targer}")
+
+
+epochs, subject_labels, labels = extract_epochs(target_type)
 
 shape = len(epochs), epochs[0].shape[1], epochs[0].shape[0]
-fname = (
-    f"nsamp_{shape[0]}-"
-    f"eplen_{epoch_duration}-"
-    f"runs_{','.join(map(str, runs))}-"
-    f"example_dset"
-)
+fname = f"nsamp_{shape[0]}-eplen_{shape[1]}-example_{target_type}"
 file = np.memmap(
     join(result_dir, "raw-" + fname + ".dat"), mode="w+", dtype=np.float32, shape=shape
 )
@@ -50,6 +81,6 @@ meta_info = pd.DataFrame(
 for i in range(shape[0]):
     file[i] = epochs[i].T
     file.flush()
-    meta_info.iloc[i] = [subject_labels[i], -1, run_labels[labels[i]]]
+    meta_info.iloc[i] = [subject_labels[i], -1, labels[i]]
 
 meta_info.to_csv(join(result_dir, "label-" + fname + ".csv"))
