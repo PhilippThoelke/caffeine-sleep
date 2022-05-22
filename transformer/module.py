5d7ad985d0616c163fcf659b7aa4eaeb78dfb57b
Philipp Thölke <philipp.thoelke@posteo.de>
2022-05-21 23:51:23 -0400

    Revert "Trainable positional encoding"
    This reverts commit e55ab2bd075c613df5a38b42d9a008ea20c43327.

--- transformer/module.py
+++ transformer/module.py
@@ -262,11 +262,23 @@ class EEGEncoder(nn.Module):
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
-        self.encodings = nn.Embedding(max_len, d_model)
 
-    def forward(self, x):
-        x = x + torch.arange(x.size(0)).view(-1, 1, 1)
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model)
+        )
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[: x.size(0)]
         return self.dropout(x)
