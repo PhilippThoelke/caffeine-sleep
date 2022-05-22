24d54e547e44acf55d99c13597fa4d720d9943ba
Philipp Thölke <philipp.thoelke@posteo.de>
2022-05-22 18:30:23 -0400

    Include all tasks

--- transformer/get_example_dset.py
+++ transformer/get_example_dset.py
@@ -7,7 +7,8 @@ from mne.datasets import eegbci
 
 
 result_dir = "transformer/data/"
-target_type = "baseline-eyes"  # baseline-eyes, fist-motion
+# baseline-eyes, fist-motion, fist-imagination, fist_feet-motion, fist_feet-imagination
+target_type = "fist-imagination"
 
 
 def extract_baseline_eyes(subjects, runs, epoch_duration):
@@ -32,7 +33,7 @@ def extract_baseline_eyes(subjects, runs, epoch_duration):
     return epochs, subject_labels, labels
 
 
-def extract_fist_motion(subjects, runs, epoch_duration):
+def extract_task(subjects, runs, epoch_duration, labels):
     epochs = []
     subject_labels = []
     labels = []
@@ -42,7 +43,11 @@ def extract_fist_motion(subjects, runs, epoch_duration):
         raw = concatenate_raws(raws)
 
         curr_epochs = mne.Epochs(
-            raw, *mne.events_from_annotations(raw), tmin=-1, tmax=epoch_duration - 1, preload=True
+            raw,
+            *mne.events_from_annotations(raw),
+            tmin=-1,
+            tmax=epoch_duration - 1,
+            preload=True,
         )
         if curr_epochs.get_data().shape[-1] != (epoch_duration * 160 + 1):
             # skip runs which don't have a sampling frequency of 160
@@ -50,11 +55,11 @@ def extract_fist_motion(subjects, runs, epoch_duration):
         # left fist
         epochs.extend(list(curr_epochs["T1"].get_data()))
         subject_labels.extend([subject] * len(curr_epochs["T1"]))
-        labels.extend(["left fist"] * len(curr_epochs["T1"]))
+        labels.extend([labels[0]] * len(curr_epochs["T1"]))
         # right fist
         epochs.extend(list(curr_epochs["T2"].get_data()))
         subject_labels.extend([subject] * len(curr_epochs["T2"]))
-        labels.extend(["right fist"] * len(curr_epochs["T2"]))
+        labels.extend([labels[1]] * len(curr_epochs["T2"]))
     return epochs, subject_labels, labels
 
 
@@ -62,7 +67,33 @@ def extract_epochs(target, subjects=range(1, 110)):
     if target == "baseline-eyes":
         return extract_baseline_eyes(subjects=subjects, runs=[1, 2], epoch_duration=5)
     elif target == "fist-motion":
-        return extract_fist_motion(subjects=subjects, runs=[3, 7, 11], epoch_duration=5)
+        return extract_task(
+            subjects=subjects,
+            runs=[3, 7, 11],
+            epoch_duration=5,
+            labels=["left fist", "right fist"],
+        )
+    elif target == "fist-imagination":
+        return extract_task(
+            subjects=subjects,
+            runs=[4, 8, 12],
+            epoch_duration=5,
+            labels=["left fist", "right fist"],
+        )
+    elif target == "fist_feet-motion":
+        return extract_task(
+            subjects=subjects,
+            runs=[5, 9, 13],
+            epoch_duration=5,
+            labels=["fists", "feet"],
+        )
+    elif target == "fist_feet-imagination":
+        return extract_task(
+            subjects=subjects,
+            runs=[6, 10, 14],
+            epoch_duration=5,
+            labels=["fists", "feet"],
+        )
     raise ValueError(f"Unrecognized target {targer}")
 
 
