20fac2376b68bfb09d6899fb4f4c54a6fc8af35c
Philipp Thölke <philipp.thoelke@posteo.de>
2022-05-22 18:32:23 -0400

    Fix ambigous variable name

--- transformer/get_example_dset.py
+++ transformer/get_example_dset.py
@@ -33,7 +33,7 @@ def extract_baseline_eyes(subjects, runs, epoch_duration):
     return epochs, subject_labels, labels
 
 
-def extract_task(subjects, runs, epoch_duration, labels):
+def extract_task(subjects, runs, epoch_duration, label_names):
     epochs = []
     subject_labels = []
     labels = []
@@ -55,11 +55,11 @@ def extract_task(subjects, runs, epoch_duration, labels):
         # left fist
         epochs.extend(list(curr_epochs["T1"].get_data()))
         subject_labels.extend([subject] * len(curr_epochs["T1"]))
-        labels.extend([labels[0]] * len(curr_epochs["T1"]))
+        labels.extend([label_names[0]] * len(curr_epochs["T1"]))
         # right fist
         epochs.extend(list(curr_epochs["T2"].get_data()))
         subject_labels.extend([subject] * len(curr_epochs["T2"]))
-        labels.extend([labels[1]] * len(curr_epochs["T2"]))
+        labels.extend([label_names[1]] * len(curr_epochs["T2"]))
     return epochs, subject_labels, labels
 
 
@@ -71,28 +71,28 @@ def extract_epochs(target, subjects=range(1, 110)):
             subjects=subjects,
             runs=[3, 7, 11],
             epoch_duration=5,
-            labels=["left fist", "right fist"],
+            label_names=["left fist", "right fist"],
         )
     elif target == "fist-imagination":
         return extract_task(
             subjects=subjects,
             runs=[4, 8, 12],
             epoch_duration=5,
-            labels=["left fist", "right fist"],
+            label_names=["left fist", "right fist"],
         )
     elif target == "fist_feet-motion":
         return extract_task(
             subjects=subjects,
             runs=[5, 9, 13],
             epoch_duration=5,
-            labels=["fists", "feet"],
+            label_names=["fists", "feet"],
         )
     elif target == "fist_feet-imagination":
         return extract_task(
             subjects=subjects,
             runs=[6, 10, 14],
             epoch_duration=5,
-            labels=["fists", "feet"],
+            label_names=["fists", "feet"],
         )
     raise ValueError(f"Unrecognized target {targer}")
 
