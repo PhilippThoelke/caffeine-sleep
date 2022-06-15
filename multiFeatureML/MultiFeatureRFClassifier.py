62418894a8e727d78c3fcbd41dc26f6f30aa3a73
Philipp Thölke <philipp.thoelke@posteo.de>
2022-06-15 17:37:04 -0400

    Add comments + formatting

--- multiFeatureML/MultiFeatureRFClassifier.py
+++ multiFeatureML/MultiFeatureRFClassifier.py
@@ -18,55 +18,63 @@ if len(sys.argv) > 1:
 if len(sys.argv) > 2:
     stage_index = int(sys.argv[2])
 
-DATA_PATH = '/home/pthoelke/projects/def-kjerbi/pthoelke/caffeine/Features{dose}/Combined'.format(dose=CAF_DOSE)
-RESULTS_PATH = '/home/pthoelke/projects/def-kjerbi/pthoelke/caffeine/results/randomForest_avg{dose}'.format(dose=CAF_DOSE)
+DATA_PATH = f"/home/pthoelke/projects/def-kjerbi/pthoelke/caffeine/Features{CAF_DOSE}/Combined"
+RESULTS_PATH = f"/home/pthoelke/projects/def-kjerbi/pthoelke/caffeine/results/randomForest_avg{CAF_DOSE}"
 
-#DATA_PATH = 'C:\\Users\\Philipp\\Documents\\Caffeine\\Features{dose}\\Combined'.format(dose=CAF_DOSE)
-#RESULTS_PATH = 'C:\\Users\\Philipp\\GoogleDrive\\Caffeine\\results\\randomForest_avg{dose}'.format(dose=CAF_DOSE)
-
-STAGES = ['AWSL', 'NREM', 'REM']
+STAGES = ["AWSL", "NREM", "REM"]
 STAGE = STAGES[stage_index]
 
 # boolean indicating whether SpecPermEn should be included
 INCLUDE_SPEC_PERM = True
 
 # load data
-with open(os.path.join(DATA_PATH, 'data_avg.pickle'), 'rb') as file:
+with open(os.path.join(DATA_PATH, "data_avg.pickle"), "rb") as file:
     data = pickle.load(file)[STAGE]
 
 # load labels
-with open(os.path.join(DATA_PATH, 'labels_avg.pickle'), 'rb') as file:
+with open(os.path.join(DATA_PATH, "labels_avg.pickle"), "rb") as file:
     y = pickle.load(file)[STAGE]
 
 # load group vectors
-with open(os.path.join(DATA_PATH, 'groups_avg.pickle'), 'rb') as file:
+with open(os.path.join(DATA_PATH, "groups_avg.pickle"), "rb") as file:
     groups = pickle.load(file)[STAGE]
 
 if INCLUDE_SPEC_PERM:
     # generate a feature name vector WITH SpecPermEn
-    feature_names = np.concatenate([[feature + '-' + str(i) for i in range(20)] for feature in data.keys()])
+    feature_names = np.concatenate(
+        [[feature + "-" + str(i) for i in range(20)] for feature in data.keys()]
+    )
 else:
     # generate a feature name vector WITHOUT SpecPermEn
-    feature_names = np.concatenate([[feature + '-' + str(i) for i in range(20)] for feature in data.keys() if 'SpecPermEn' not in feature])
+    feature_names = np.concatenate(
+        [
+            [feature + "-" + str(i) for i in range(20)]
+            for feature in data.keys()
+            if "SpecPermEn" not in feature
+        ]
+    )
 
-print(f'Multi feature random forest, include SpecPermEn: {INCLUDE_SPEC_PERM}, CAF{CAF_DOSE}, stage {STAGE}')
+print(
+    f"Multi feature random forest, include SpecPermEn: {INCLUDE_SPEC_PERM}, CAF{CAF_DOSE}, stage {STAGE}"
+)
 
 x = []
 # create a sample matrix from the data dict
 for feature in data.keys():
     if not INCLUDE_SPEC_PERM:
-        if 'SpecPermEn' in feature:
+        if "SpecPermEn" in feature:
             # skip SpecPermEn if INCLUDE_SPEC_PERM is false
             continue
     x.append(data[feature])
 x = np.concatenate(x, axis=1)
 
-print(f'Found {len(x)} samples')
+print(f"Found {len(x)} samples")
 
 # leave P groups out as testing data
 cv = model_selection.LeavePGroupsOut(n_groups=5)
 # make a list out of the data split generator for random access
 cv_split = list(cv.split(x, y, groups))
+
 
 def train(train, test):
     # initialize a random forest
@@ -74,61 +82,77 @@ def train(train, test):
 
     # generate a parameter dict for the random forest during grid search
     params = {
-        'n_estimators': [75, 100, 150, 200],
-        'max_depth': [None, 50, 75],
-        'criterion': ['gini', 'entropy'],
-        'max_features': ['sqrt', 'log2'],
-        'min_samples_leaf': [1, 5, 10],
-        'bootstrap': [True, False],
-        'n_jobs': [-1]
+        "n_estimators": [75, 100, 150, 200],
+        "max_depth": [None, 50, 75],
+        "criterion": ["gini", "entropy"],
+        "max_features": ["sqrt", "log2"],
+        "min_samples_leaf": [1, 5, 10],
+        "bootstrap": [True, False],
+        "n_jobs": [-1],
     }
 
     # generate a K-fold split of the training subset for cross validation during the grid search
     kfold_inner = model_selection.GroupKFold(n_splits=7)
-    inner_cross_validation_split = kfold_inner.split(x[train],
-                                                     y[train],
-                                                     groups[train])
+    inner_cross_validation_split = kfold_inner.split(x[train], y[train], groups[train])
 
     # run grid search
-    grid_search = model_selection.GridSearchCV(estimator=clf,
-                                               param_grid=params,
-                                               cv=inner_cross_validation_split,
-                                               iid=False,
-                                               refit=True,
-                                               n_jobs=-1)
+    grid_search = model_selection.GridSearchCV(
+        estimator=clf,
+        param_grid=params,
+        cv=inner_cross_validation_split,
+        iid=False,
+        refit=True,
+        n_jobs=-1,
+    )
     grid_search.fit(x[train], y[train], groups[train])
 
-    return grid_search.best_estimator_.feature_importances_, grid_search.best_estimator_.score(x[test], y[test])
+    return (
+        grid_search.best_estimator_.feature_importances_,
+        grid_search.best_estimator_.score(x[test], y[test]),
+    )
 
+
+# perform grid search and training n times
 perm = np.random.permutation(len(cv_split))
-results = Parallel(n_jobs=15)(delayed(train)(*cv_split[perm[i]]) for i in range(iterations))
+results = Parallel(n_jobs=15)(
+    delayed(train)(*cv_split[perm[i]]) for i in range(iterations)
+)
 
+# extract importances and accuracies for the n random forests
 importances = [result[0] for result in results]
 scores = [result[1] for result in results]
 
-print('mean score:', np.mean(scores), '\n', flush=True)
+print("mean score:", np.mean(scores), "\n", flush=True)
 
 if INCLUDE_SPEC_PERM:
     # save the trained estimators
-    with open(os.path.join(RESULTS_PATH, f'specpermen_importances-{STAGE}.pickle'), 'wb') as file:
+    with open(
+        os.path.join(RESULTS_PATH, f"specpermen_importances-{STAGE}.pickle"), "wb"
+    ) as file:
         pickle.dump(importances, file)
 
     # save the testing data corresponding to each of the estimators
-    with open(os.path.join(RESULTS_PATH, f'specpermen_scores-{STAGE}.pickle'), 'wb') as file:
+    with open(
+        os.path.join(RESULTS_PATH, f"specpermen_scores-{STAGE}.pickle"), "wb"
+    ) as file:
         pickle.dump(scores, file)
 
     # save the feature name vector
-    with open(os.path.join(RESULTS_PATH, f'specpermen_feature_names-{STAGE}.pickle'), 'wb') as file:
+    with open(
+        os.path.join(RESULTS_PATH, f"specpermen_feature_names-{STAGE}.pickle"), "wb"
+    ) as file:
         pickle.dump(feature_names, file)
 else:
     # save the trained estimators
-    with open(os.path.join(RESULTS_PATH, f'importances-{STAGE}.pickle'), 'wb') as file:
+    with open(os.path.join(RESULTS_PATH, f"importances-{STAGE}.pickle"), "wb") as file:
         pickle.dump(importances, file)
 
     # save the testing data corresponding to each of the estimators
-    with open(os.path.join(RESULTS_PATH, f'scores-{STAGE}.pickle'), 'wb') as file:
+    with open(os.path.join(RESULTS_PATH, f"scores-{STAGE}.pickle"), "wb") as file:
         pickle.dump(scores, file)
 
     # save the feature name vector
-    with open(os.path.join(RESULTS_PATH, f'feature_names-{STAGE}.pickle'), 'wb') as file:
+    with open(
+        os.path.join(RESULTS_PATH, f"feature_names-{STAGE}.pickle"), "wb"
+    ) as file:
         pickle.dump(feature_names, file)
