222a04447d40cbb52c4c9d630a28ec729a0171fc
Philipp Thölke <philipp.thoelke@posteo.de>
2022-07-06 10:39:04 -0400

    Wrap code in main function

--- singleFeatureML/SingleFeatureML-Classifier.py
+++ singleFeatureML/SingleFeatureML-Classifier.py
@@ -12,6 +12,7 @@ from sklearn import (
     gaussian_process,
 )
 
+
 CAF_DOSE = 200
 SIGNIFICANT_P = 0.05
 
@@ -35,85 +36,92 @@ RESULTS_PATH = "results"
 STAGES = ["AWSL", "NREM", "REM"]
 BANDS = ["delta", "theta", "alpha", "sigma", "beta", "low gamma"]
 
-with open(os.path.join(DATA_PATH, "data_avg.pickle"), "rb") as file:
-    data = pickle.load(file)
-with open(os.path.join(DATA_PATH, "labels_avg.pickle"), "rb") as file:
-    labels = pickle.load(file)
-with open(os.path.join(DATA_PATH, "groups_avg.pickle"), "rb") as file:
-    groups = pickle.load(file)
-
 
 def get_classifier(name, **kwargs):
-    if CLASSIFIER.lower() == "svm":
+    if name.lower() == "svm":
         return svm.SVC(gamma="auto", **kwargs)
-    elif CLASSIFIER.lower() == "lda":
+    elif name.lower() == "lda":
         return discriminant_analysis.LinearDiscriminantAnalysis(**kwargs)
-    elif CLASSIFIER.lower() == "qda":
+    elif name.lower() == "qda":
         return discriminant_analysis.QuadraticDiscriminantAnalysis(**kwargs)
-    elif CLASSIFIER.lower() == "gradientboosting":
+    elif name.lower() == "gradientboosting":
         return ensemble.GradientBoostingClassifier(**kwargs)
-    elif CLASSIFIER.lower() == "kneighbors":
+    elif name.lower() == "kneighbors":
         return neighbors.KNeighborsClassifier(n_jobs=-1, **kwargs)
-    elif CLASSIFIER.lower() == "adaboost":
+    elif name.lower() == "adaboost":
         return ensemble.AdaBoostClassifier(**kwargs)
-    elif CLASSIFIER.lower() == "gaussianprocess":
+    elif name.lower() == "gaussianprocess":
         return gaussian_process.GaussianProcessClassifier(n_jobs=-1, **kwargs)
-    elif CLASSIFIER.lower() == "perceptron":
+    elif name.lower() == "perceptron":
         return linear_model.Perceptron(n_jobs=-1, **kwargs)
 
 
-assert len(sys.argv) > 1, (
-    f"please provide the index of the classifier to train as a command line argument: "
-    f"{dict(zip(range(len(CLASSIFIERS)),CLASSIFIERS))}"
-)
-clf_id = int(sys.argv[1])
-CLASSIFIER = CLASSIFIERS[clf_id]
+def main():
+    with open(os.path.join(DATA_PATH, "data_avg.pickle"), "rb") as file:
+        data = pickle.load(file)
+    with open(os.path.join(DATA_PATH, "labels_avg.pickle"), "rb") as file:
+        labels = pickle.load(file)
+    with open(os.path.join(DATA_PATH, "groups_avg.pickle"), "rb") as file:
+        groups = pickle.load(file)
 
-print(f"Decoding accuracy average, {CLASSIFIER} on CAF {CAF_DOSE}")
+    assert len(sys.argv) > 1, (
+        f"please provide the index of the classifier to train as a command line argument: "
+        f"{dict(zip(range(len(CLASSIFIERS)),CLASSIFIERS))}"
+    )
+    clf_id = int(sys.argv[1])
+    CLASSIFIER = CLASSIFIERS[clf_id]
 
-scores = {}
-for stage in STAGES:
-    scores[stage] = {}
-    print(f"Sleep stage {stage}")
-    for feature in data[stage].keys():
-        scores[stage][feature] = []
-        for electrode in range(20):
-            print(
-                f"   {CLASSIFIER} on {feature}, elec {electrode + 1}",
-                end="",
-                flush=True,
-            )
+    print(f"Decoding accuracy average, {CLASSIFIER} on CAF {CAF_DOSE}")
 
-            x = data[stage][feature][:, electrode].reshape((-1, 1))
-            y = labels[stage]
-            g = groups[stage]
-            print(f", permutation test on {len(x)} samples", end="")
+    scores = {}
+    for stage in STAGES:
+        scores[stage] = {}
+        print(f"Sleep stage {stage}")
+        for feature in data[stage].keys():
+            scores[stage][feature] = []
+            for electrode in range(20):
+                print(
+                    f"   {CLASSIFIER} on {feature}, elec {electrode + 1}",
+                    end="",
+                    flush=True,
+                )
 
-            # train classifier
-            kfold = model_selection.GroupKFold(n_splits=10)
-            score = model_selection.permutation_test_score(
-                estimator=get_classifier(CLASSIFIER),
-                n_permutations=1000,
-                X=x,
-                y=y,
-                groups=g,
-                cv=kfold.split(X=x, y=y, groups=g),
-                n_jobs=-1,
-            )
-            print(f", score: {score[0]}, pvalue: {score[2]}")
-            scores[stage][feature].append(score)
-        print()
+                x = data[stage][feature][:, electrode].reshape((-1, 1))
+                y = labels[stage]
+                g = groups[stage]
+                print(f", permutation test on {len(x)} samples", end="")
 
-path = os.path.join(RESULTS_PATH, f"singleML{CAF_DOSE}", f"scores_{CLASSIFIER}.pickle")
-with open(path, "wb",) as file:
-    pickle.dump(scores, file)
+                # train classifier
+                kfold = model_selection.GroupKFold(n_splits=10)
+                score = model_selection.permutation_test_score(
+                    estimator=get_classifier(CLASSIFIER),
+                    n_permutations=1000,
+                    X=x,
+                    y=y,
+                    groups=g,
+                    cv=kfold.split(X=x, y=y, groups=g),
+                    n_jobs=-1,
+                )
+                print(f", score: {score[0]}, pvalue: {score[2]}")
+                scores[stage][feature].append(score)
+            print()
 
-all_scores = [
-    [[elec[0] for elec in ft] for ft in stage.values()] for stage in scores.values()
-]
-vmin = np.min(all_scores)
-vmax = np.max(all_scores)
+    path = os.path.join(
+        RESULTS_PATH, f"singleML{CAF_DOSE}", f"scores_{CLASSIFIER}.pickle"
+    )
+    with open(path, "wb",) as file:
+        pickle.dump(scores, file)
 
-print(f"Min accuracy: {vmin * 100:.2f}%")
-print(f"Max accuracy: {vmax * 100:.2f}%")
-print(f"Mean accuracy: {np.mean(all_scores) * 100:.2f}%")
+    all_scores = [
+        [[elec[0] for elec in ft] for ft in stage.values()] for stage in scores.values()
+    ]
+    vmin = np.min(all_scores)
+    vmax = np.max(all_scores)
+
+    print(f"Min accuracy: {vmin * 100:.2f}%")
+    print(f"Max accuracy: {vmax * 100:.2f}%")
+    print(f"Mean accuracy: {np.mean(all_scores) * 100:.2f}%")
+
+
+if __name__ == "__main__":
+    main()
